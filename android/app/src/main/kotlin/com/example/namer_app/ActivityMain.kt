476ff59bf1665357c900727459d9041b52e345cf
heyzec <61238538+heyzec@users.noreply.github.com>
2024-01-03 22:29:06 +0800

    at least its buildable

---
+++ android/app/src/main/kotlin/com/example/namer_app/ActivityMain.kt
@@ -0,0 +1,14 @@
+package com.example.namer_app
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.CompoundButton
+import com.joaomgcd.taskerpluginlibrary.extensions.requestQuery
+
+
+class ActivityMain : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
