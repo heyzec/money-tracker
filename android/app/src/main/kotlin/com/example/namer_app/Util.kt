e53e66a3fde779bb47508bc1b57391feb23bcbb0
heyzec <61238538+heyzec@users.noreply.github.com>
2024-01-03 22:43:03 +0800

    lessen res

--- android/app/src/main/kotlin/com/example/namer_app/Util.kt
+++ android/app/src/main/kotlin/com/example/namer_app/Util.kt
@@ -18,17 +18,6 @@ fun String.toToast(context: Context) {
     Handler(Looper.getMainLooper()).post { Toast.makeText(context, this, Toast.LENGTH_LONG).show() }
 }
 
-fun Activity.selectOne(title: String, options: List<String>, callback: (String?) -> Unit) {
-    AlertDialog.Builder(this).apply {
-        setIcon(R.mipmap.ic_launcher)
-        setTitle(title)
-        val arrayAdapter = ArrayAdapter<String>(this@selectOne, android.R.layout.select_dialog_singlechoice).apply {
-            addAll(options)
-        }
-        setAdapter(arrayAdapter) { _, which -> callback(arrayAdapter.getItem(which)) }
-        setNegativeButton("Cancel") { dialog, _ -> dialog.dismiss(); callback(null) }
-    }.show()
-}
 
 fun Activity.alert(title: String, message: String) {
     val alertDialog = AlertDialog.Builder(this).create()
