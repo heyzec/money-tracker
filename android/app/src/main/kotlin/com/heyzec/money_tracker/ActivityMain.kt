bbc5eaa6dddeb117f61a6b4112ea13a828df46e6
heyzec <61238538+heyzec@users.noreply.github.com>
2024-01-04 14:23:53 +0800

    feat: add hello world tasker plugin
    - adapted from https://github.com/joaomgcd/TaskerPluginSample

---
+++ android/app/src/main/kotlin/com/heyzec/money_tracker/ActivityMain.kt
@@ -0,0 +1,14 @@
+package com.heyzec.money_tracker
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.CompoundButton
+import com.joaomgcd.taskerpluginlibrary.extensions.requestQuery
+
+
+class ActivityMain : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
