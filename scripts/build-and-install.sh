c9c06db06f8782f056c009b8ae5dad95f61df40f
heyzec <61238538+heyzec@users.noreply.github.com>
2024-01-04 14:24:21 +0800

    feat: add proof of concept tasker-flutter plugin
    - adapted from https://github.com/k9withabone/flutter_tasker_integration_example

---
+++ scripts/build-and-install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# build-and-install.sh
+# Builds a debug APK and installs it via adb
+
+flutter build apk --debug
+adb install build/app/outputs/flutter-apk/app-debug.apk
+adb shell -- cmd notification post -S bigtext -t "'Installed debug app'" 'tag' "'Installed on $(date)'"
